aa0fc9574e05ab9ef9196856f4c82c9ca2e70ef0
adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2024-03-06 16:17:58 -0300

    estudando POO em TY

--- CursoTypeScript/classes/classes.js
+++ CursoTypeScript/classes/classes.js
@@ -60,7 +60,103 @@ class Carro {
             this.velocidadeAtual = novaVelocidade;
         }
         else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(20);
+    }
+    frear() {
+        return this.alterarVelocidade(-15);
+    }
+}
+const carro1 = new Carro('for', 'ka', 23);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+class Ferrari extends Carro {
+    constructor(modelo, velocidadeMaxima) {
+        super('ferr', modelo, velocidadeMaxima);
+    }
+}
+const f40 = new Ferrari("dd", 344);
+console.log(`${f40.marca} ${f40.modelo}`);
+console.log(f40.acelerar());
+console.log(f40.frear());
+//Get e Set
+class Pessoa {
+    constructor() {
+        this._idade = 0;
+    }
+    get idade() {
+        return this._idade;
+    }
+    set idade(valor) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor;
         }
     }
 }
+const pessoa1 = new Pessoa;
+pessoa1.idade = 10;
+console.log(pessoa1.idade);
+pessoa1.idade = -3;
+console.log(pessoa1.idade);
+// Atributos e métodos estáticos
+class Matematica {
+    constructor() {
+        this.PI = 3.1416;
+    }
+    areaCirc(raio) {
+        return this.PI * raio * raio;
+    }
+}
+const m1 = new Matematica();
+m1.PI = 4.2;
+console.log(m1.areaCirc(4));
+//console.log(Matematica().areaCirc(4))
+// Classe abstrata
+class Calculo {
+    constructor() {
+        this.resultado = 0;
+    }
+    getResultado() {
+        return this.resultado;
+    }
+}
+//console.log(new X())
+class Soma extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t + a);
+    }
+}
+class Multiplicacao extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t * a);
+    }
+}
+let c1 = new Soma();
+c1.executar(2, 3, 4, 5);
+console.log(c1.getResultado());
+class Unico {
+    constructor() { }
+    static getInstance() {
+        return Unico.instance;
+    }
+    agora() {
+        return new Date;
+    }
+}
+Unico.instance = new Unico;
+//const errado = new Unico()
+console.log(Unico.getInstance().agora());
+// Somente Leitura
+class Avião {
+    constructor(modelo, prefixo) {
+        this.prefixo = prefixo;
+        this.modelo = modelo;
+    }
+}
+const turboHelice = new Aviao('hdguduh', 'jdbisn');
+turboHelice.modelo = 'dad';
 //# sourceMappingURL=classes.js.map
